0f58ab15de8c39d58bfa53afb23655c6c437d0fd
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-16 08:16:03 -0400

    Persistan Vuex State/Store

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <p>You chose {{ $store.getters.sessionToken }}</p>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'home',
+  name: "home"
 };
 </script>
