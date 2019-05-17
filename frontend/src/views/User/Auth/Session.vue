91119ac2b0b31f341fc537bda6d794e33e04f67d
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-17 18:55:46 -0400

    Updated to use this.callAPI();

--- frontend/src/views/User/Auth/Session.vue
+++ frontend/src/views/User/Auth/Session.vue
@@ -25,63 +25,18 @@ export default {
   },
   methods: {
     async removeSession(token) {
-      try {
-        const logoutResponse = await (await fetch(
-          "http://localhost:3000/api/v1/auth/logout",
-          {
-            method: "post",
-            body: JSON.stringify({ token }),
-            headers: {
-              "Content-Type": "application/json"
-            }
-          }
-        )).json();
-        this.sessions = this.sessions.filter(e => e.token !== token);
-        this.$swal({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          type: logoutResponse.status,
-          title: logoutResponse.message
-        });
-        if (this.sessions.length === 0) {
-          this.$store.commit("setSessionToken", undefined);
-          this.$router.push("/");
-        }
-      } catch (error) {
-        this.$swal({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          type: "error",
-          title: "API Error"
-        });
+      await this.callAPI("auth/logout", "post", { token });
+      this.sessions = this.sessions.filter(e => e.token !== token);
+      if (this.sessions.length === 0) {
+        this.$store.commit("setSessionToken", undefined);
+        this.$router.push("/");
       }
     },
     async fetchSessions() {
-      try {
-        this.sessions = await (await fetch(
-          "http://localhost:3000/api/v1/auth/sessions",
-          {
-            method: "post",
-            body: JSON.stringify({ token: this.$store.getters.sessionToken }),
-            headers: {
-              "Content-Type": "application/json"
-            }
-          }
-        )).json();
-      } catch (error) {
-        this.$swal({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          type: "error",
-          title: "API Error"
-        });
-      }
+      const sessions = await this.callAPI("auth/sessions", "post", {
+        token: this.$store.getters.sessionToken
+      });
+      if (sessions) this.sessions = sessions;
     }
   }
 };
