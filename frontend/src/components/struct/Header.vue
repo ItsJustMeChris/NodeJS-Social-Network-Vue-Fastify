608eab2bbf80231f17317ff52004ae885108ee82
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-17 18:11:56 -0400

    Added /register to links.

--- frontend/src/components/struct/Header.vue
+++ frontend/src/components/struct/Header.vue
@@ -4,6 +4,7 @@
       <router-link class="link" to="/">Home</router-link>
       <router-link class="link" to="/about">About</router-link>
       <router-link class="link" to="/login" v-if="!$store.getters.sessionToken">Login</router-link>
+      <router-link class="link" to="/register" v-if="!$store.getters.sessionToken">Register</router-link>
       <router-link class="link" to="/sessions" v-if="$store.getters.sessionToken">Sessions</router-link>
       <router-link class="link" to="/logout" v-if="$store.getters.sessionToken">Logout</router-link>
     </main>
