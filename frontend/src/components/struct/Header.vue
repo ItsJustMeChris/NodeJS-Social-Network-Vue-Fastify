b9e306aec1c20f84d1652eae295b0a32cb1bf4be
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-17 15:54:13 -0400

    Added class to links in header for styling

--- frontend/src/components/struct/Header.vue
+++ frontend/src/components/struct/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <header>
     <main>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/login" v-if="!$store.getters.sessionToken">Login</router-link>
-      <router-link to="/sessions" v-if="$store.getters.sessionToken">Sessions</router-link>
-      <router-link to="/logout" v-if="$store.getters.sessionToken">Logout</router-link>
+      <router-link class="link" to="/">Home</router-link>
+      <router-link class="link" to="/about">About</router-link>
+      <router-link class="link" to="/login" v-if="!$store.getters.sessionToken">Login</router-link>
+      <router-link class="link" to="/sessions" v-if="$store.getters.sessionToken">Sessions</router-link>
+      <router-link class="link" to="/logout" v-if="$store.getters.sessionToken">Logout</router-link>
     </main>
   </header>
 </template>
@@ -15,4 +15,7 @@ export default {};
 </script>
 
 <style>
+.link {
+  margin-right: 1rem;
+}
 </style>
