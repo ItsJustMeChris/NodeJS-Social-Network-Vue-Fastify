8fa2617bbf6b71ca1f8536fb895e0ee6130dd6cf
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-17 18:50:14 -0400

    Added this.callAPI();

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,46 @@ import store from './store';
 Vue.use(VueSweetalert2);
 Vue.config.productionTip = false;
 
+Vue.mixin({
+  methods: {
+    async callAPI(endpoint, method, params) {
+      try {
+        const response = await (await fetch(
+          `http://localhost:3000/api/v1/${endpoint}`,
+          {
+            method,
+            body: JSON.stringify(params),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        )).json();
+        if (response.status && response.message) {
+          this.$swal({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            type: response.status,
+            title: response.message,
+          });
+        }
+        return response;
+      } catch (error) {
+        this.$swal({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          type: 'error',
+          title: 'API Error',
+        });
+        return null;
+      }
+    },
+  },
+});
+
 new Vue({
   router,
   store,
