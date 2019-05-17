53578ce1d05f1ddceba224ec82716e954502ade1
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-17 18:50:22 -0400

    Update to use this.callAPI();

--- frontend/src/components/User/Auth/Logout.vue
+++ frontend/src/components/User/Auth/Logout.vue
@@ -12,46 +12,11 @@ export default {
   methods: {
     async logout() {
       const token = this.$store.getters.sessionToken;
-
-      this.$router.replace(
-        "/",
-        async () => {
-          this.$store.commit("setSessionToken", undefined);
-          try {
-            console.log(token);
-            const logoutResponse = await (await fetch(
-              "http://localhost:3000/api/v1/auth/logout",
-              {
-                method: "post",
-                body: JSON.stringify({
-                  token
-                }),
-                headers: {
-                  "Content-Type": "application/json"
-                }
-              }
-            )).json();
-            this.$swal({
-              toast: true,
-              position: "top-end",
-              showConfirmButton: false,
-              timer: 3000,
-              type: logoutResponse.status,
-              title: logoutResponse.message
-            });
-          } catch (error) {
-            this.$swal({
-              toast: true,
-              position: "top-end",
-              showConfirmButton: false,
-              timer: 3000,
-              type: "error",
-              title: "API Error"
-            });
-          }
-        },
-        () => {}
-      );
+      await this.callAPI("auth/logout", "post", {
+        token
+      });
+      this.$store.commit("setSessionToken", undefined);
+      this.$router.push("/");
     }
   }
 };
