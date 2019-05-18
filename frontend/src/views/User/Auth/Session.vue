f1cfd73e9e8b1c6925b27df53315b32b541d030b
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-18 19:17:25 -0400

    Update Session.vue
    Star Component(izing) everything.

--- frontend/src/views/User/Auth/Session.vue
+++ frontend/src/views/User/Auth/Session.vue
@@ -1,43 +1,15 @@
 <template>
   <div>
-    Sessions:
-    <div v-for="(session, index) in sessions" :key="index">
-      {{ session }}
-      <div v-if="session.token === $store.getters.sessionToken">Current</div>
-      <button @click="removeSession(session.token)">Remove</button>
-    </div>
+    <Sessions/>
   </div>
 </template>
 
 <script>
+import Sessions from "../../../components/User/Auth/Sessions";
+
 export default {
-  name: "session",
-  data() {
-    return {
-      sessions: []
-    };
-  },
-  beforeMount() {
-    this.fetchSessions();
-  },
-  watch: {
-    $route: "fetchSessions"
-  },
-  methods: {
-    async removeSession(token) {
-      await this.callAPI("auth/logout", "post", { token });
-      this.sessions = this.sessions.filter(e => e.token !== token);
-      if (this.sessions.length === 0) {
-        this.$store.commit("setSessionToken", undefined);
-        this.$router.push("/");
-      }
-    },
-    async fetchSessions() {
-      const sessions = await this.callAPI("auth/sessions", "post", {
-        token: this.$store.getters.sessionToken
-      });
-      if (sessions) this.sessions = sessions;
-    }
+  components: {
+    Sessions
   }
 };
 </script>
