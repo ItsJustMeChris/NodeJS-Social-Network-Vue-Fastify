bb1f0a16e365cfcbaab40faaa0ccd69c57ec96a7
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-18 19:17:22 -0400

    Create Sessions.vue
    Star Component(izing) everything.

---
+++ frontend/src/components/User/Auth/Sessions.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    Sessions:
+    <div v-for="(session, index) in sessions" :key="index">
+      {{ session }}
+      <div v-if="session.token === $store.getters.sessionToken">Current</div>
+      <button @click="removeSession(session.token)">Remove</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sessions: []
+    };
+  },
+  beforeMount() {
+    this.fetchSessions();
+  },
+  watch: {
+    $route: "fetchSessions"
+  },
+  methods: {
+    async removeSession(token) {
+      await this.callAPI("auth/logout", "post", { token });
+      this.sessions = this.sessions.filter(e => e.token !== token);
+      if (this.sessions.length === 0) {
+        this.$store.commit("setSessionToken", undefined);
+        this.$router.push("/");
+      }
+    },
+    async fetchSessions() {
+      const sessions = await this.callAPI("auth/sessions", "post", {
+        token: this.$store.getters.sessionToken
+      });
+      if (sessions) this.sessions = sessions;
+    }
+  }
+};
+</script>
