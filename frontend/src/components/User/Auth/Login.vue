129ec17a81fb1e93af80d293ef101068ef587de6
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-18 19:15:08 -0400

    Create Login.vue
    Start component(izing) everything.

---
+++ frontend/src/components/User/Auth/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" v-model="username" placeholder="Username">
+    <input type="password" placeholder="Password" v-model="password">
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: null,
+      password: null
+    };
+  },
+  methods: {
+    async login() {
+      const { username, password } = this;
+      const authResponse = await this.callAPI("auth/login", "post", {
+        username,
+        password
+      });
+      if (authResponse.token)
+        this.$store.commit("setSessionToken", authResponse.token);
+      this.$router.push("/");
+    }
+  }
+};
+</script>
