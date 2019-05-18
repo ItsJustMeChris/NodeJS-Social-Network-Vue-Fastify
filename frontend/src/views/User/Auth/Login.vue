f0fd1c6bd2c140def79467358955f52c09213734
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-05-18 19:15:14 -0400

    Update Login.vue
    Start component(izing) everything.

--- frontend/src/views/User/Auth/Login.vue
+++ frontend/src/views/User/Auth/Login.vue
@@ -5,53 +5,30 @@
       <button @click="selection = 'register'">Register</button>
     </div>
     <div v-if="selection === 'login'">
-      <input type="text" v-model="username" placeholder="Username">
-      <input type="password" placeholder="Password" v-model="password">
-      <button @click="login">Login</button>
+      <Login/>
     </div>
     <div v-if="selection === 'register'">
-      <input type="text" v-model="username" placeholder="Username">
-      <input type="text" v-model="email" placeholder="Email">
-      <input type="password" placeholder="Password" v-model="password">
-      <button @click="regiser">Register</button>
+      <Register/>
     </div>
   </div>
 </template>
 
 <script>
+import Login from "../../../components/User/Auth/Login";
+import Register from "../../../components/User/Auth/Register";
+
 export default {
   name: "login",
   props: { requestedAction: String },
+  components: {
+    Login,
+    Register
+  },
   data() {
     return {
-      username: null,
-      password: null,
-      email: null,
       selection: this.requestedAction
     };
   },
-  methods: {
-    async login() {
-      const { username, password } = this;
-      const authResponse = await this.callAPI("auth/login", "post", {
-        username,
-        password
-      });
-      if (authResponse.token)
-        this.$store.commit("setSessionToken", authResponse.token);
-      this.$router.push("/");
-    },
-    async regiser() {
-      const { username, password, email } = this;
-      const authResponse = await this.callAPI("auth/register", "post", {
-        username,
-        password,
-        email
-      });
-      if (authResponse.token)
-        this.$store.commit("setSessionToken", authResponse.token);
-      this.$router.push("/");
-    }
-  }
+  methods: {}
 };
 </script>
